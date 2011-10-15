9b5670dac86dd53090ece7ce9d7c125932d34584
Menan Vadivel <menan89@gmail.com>
2011-10-15 17:42:16 -0400

    mail servers added

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Bidding::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+:address => 'smtp.gmail.com',
+:port => 587,
+:domain => 'pennybid.heroku.com',
+:authentication => :plain,
+:user_name => 'menan89',
+:password => 'Kadavul_123'
+}
