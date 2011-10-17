7d696787e32363d5372b68fb65775776ce5f49a7
Menan Vadivel <menan89@gmail.com>
2011-10-16 20:44:50 -0400

    user registration fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  	validates :password, :confirmation => true
+  	validates :password, :confirmation => true, :presence => true, :on => :update
+  	validates :name, :presence => true, :allow_nil => true, :on => :update
 	validates :email, :presence =>true, :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => "is invalid"}, :uniqueness=>true
 	has_many :bids, :dependent => :destroy
 
